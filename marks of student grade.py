ab3e5b4308f4909c7d78c9c2ff35bbe26f5f80c0
Sumit chaudhari <sumitpatil0938@gmail.com>
2026-06-13 14:07:32 +0530

    Add files via upload

---
+++ marks of student grade.py
@@ -0,0 +1,13 @@
+#  MARKS 5 STUDENT
+print("done with the loop")
+student = str(input("enter student name :"))
+marks = int(input("enter student marks :"))
+print(student ,marks)
+if(marks>=90):
+ print("grade A")
+elif(marks>=75):
+  print("grade B")
+elif(marks>=60):
+  print("grade C")
+else:
+  print("fail")
